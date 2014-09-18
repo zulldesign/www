eb07caf91b7e31627337cfad68e279ad22a28b72
zulldesign <admin@zulldesign.ml>
2014-09-18 15:03:27 +0800

    l

---
+++ www/www/Controllers/MvcController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace www.Controllers
+{
+    public class MvcController : Controller
+    {
+        //
+        // GET: /Mvc/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Mvc/Welcome/ 
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+    }
+}
---
+++ www/www/Controllers/TutorialsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace www.Controllers
+{
+    public class TutorialsController : Controller
+    {
+        //
+        // GET: /Tutorials/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Tutorials/Welcome/ 
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+    }
+}
