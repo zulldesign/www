5c2ce5536e5fa2c56f3ded4c1cc0ebec41ea3839
zulldesign <admin@zulldesign.ml>
2014-09-20 12:35:03 +0800

    l

--- www/www/Controllers/MoviesController.cs
+++ www/www/Controllers/MoviesController.cs
@@ -119,5 +119,21 @@ namespace www.Controllers
             db.Dispose();
             base.Dispose(disposing);
         }
+
+        //
+        // GET: /Movies/SearchIndex
+
+        public ActionResult SearchIndex(string searchString)
+        {
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(movies);
+        }
     }
 }
