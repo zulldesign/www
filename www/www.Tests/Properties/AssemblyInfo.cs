e82d984d3427f4084d2dc38b4798523ba75f7605
zulldesign <admin@zulldesign.ml>
2014-09-24 16:06:08 +0800

    l

--- www/www.Tests/Properties/AssemblyInfo.cs
+++ www/www.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("71fc6918-8260-44df-b4de-9e039915aa61")]
+[assembly: Guid("148babb2-b757-48cb-bdab-a79c919df3df")]
 
 // Version information for an assembly consists of the following four values:
 //
--- www/www/Properties/AssemblyInfo.cs
+++ www/www/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ceac5e62-e60f-435f-9695-0e14090c30c5")]
+[assembly: Guid("21fd561d-226c-4934-a3d4-b3309278767a")]
 
 // Version information for an assembly consists of the following four values:
 //
