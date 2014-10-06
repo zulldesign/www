1a593f3552134c9624cead853909e315204315dc
zulldesign <admin@zulldesign.ml>
2014-10-07 01:34:36 +0800

    l

--- www/www/Controllers/HomeController.cs
+++ www/www/Controllers/HomeController.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Mvc;
-using Microsoft.AspNet.Mvc.Facebook;
-using Microsoft.AspNet.Mvc.Facebook.Client;
-using www.Models;
+﻿using System.Web.Mvc;
 
-namespace www.Controllers
+namespace MvcApplication1.Controllers
 {
+    [HandleError]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View(Zulfahmy-Mokhtar);
         }
     }
 }
