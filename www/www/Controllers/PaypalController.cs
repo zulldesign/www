5fe32180f18e2e61ac822b1cb6af57489031a0a0
zulldesign <admin@zulldesign.ml>
2014-10-07 03:04:08 +0800

    l

--- www/www/Controllers/PaypalController.cs
+++ www/www/Controllers/PaypalController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using www.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,33 @@ namespace www.Controllers
 {
     public class PaypalController : Controller
     {
-        //
-        // GET: /Paypal/
+        //[Authorize(Roles="Customers")]
+        public ActionResult ValidateCommand(string product, string totalPrice)
+        {
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSandbox"]);
+            var Paypal = new PaypalModel(useSandbox);
 
+            Paypal.item_name = product;
+            Paypal.amount = totalPrice;
+            return View(Paypal);
+        }
+
+        public ActionResult RedirectFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult CancelFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult NotifyFromPaypal()
+        {
+            return View();
+        }
+
+        //<add key="business" value="asrce2_1311074442_biz@gmail.com" />
         public ActionResult Index()
         {
             return View();
