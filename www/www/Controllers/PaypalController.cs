8aeb3503f998b941a20756a2ba83a5e6d7863ae6
zulldesign <admin@zulldesign.ml>
2014-10-07 00:41:33 +0800

    l

---
+++ www/www/Controllers/PaypalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace www.Controllers
+{
+    public class PaypalController : Controller
+    {
+        //
+        // GET: /Paypal/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
