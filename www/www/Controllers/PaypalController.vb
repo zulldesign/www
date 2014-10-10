71171e9452dce29ee1961deb7a7f08c385889551
zulldesign <admin@zulldesign.ml>
2014-10-10 21:55:28 +0800

    l

---
+++ www/www/Controllers/PaypalController.vb
@@ -0,0 +1,11 @@
+﻿Public Class PaypalController
+    Inherits System.Web.Mvc.Controller
+
+    '
+    ' GET: /Paypal
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+End Class
