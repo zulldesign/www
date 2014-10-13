e87de5eecfd297d28ca04a650263bcd17e24a347
zulldesign <admin@zulldesign.ml>
2014-10-13 08:52:00 +0800

    l

--- www/www/Controllers/ProductController.cs
+++ www/www/Controllers/ProductController.cs
@@ -1,8 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace www.Controllers
-{
-    [HandleError]
+{    
     public class ProductController : Controller
     {
         public ActionResult Index()
