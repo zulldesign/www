bf517c286511059e26bf8798b2b3bb2f6f5a4f1a
zulldesign <admin@zulldesign.ml>
2014-10-20 11:29:41 +0800

    l

--- www/www/Controllers/HomeController.cs
+++ www/www/Controllers/HomeController.cs
@@ -8,16 +8,13 @@ namespace www.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Home/
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
             return View();
         }
 
-        public ActionResult About()
-        {
-            return View();
-        }
     }
 }
