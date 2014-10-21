d6a312b9de6ac84283c704d48a3ee462be37c6ed
zulldesign <admin@zulldesign.ml>
2014-10-21 09:29:48 +0800

    l

---
+++ www/www/Controllers/KNPR2C9H4G4KQ6MHC4YKM96XRController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace www.Controllers
+{
+    public class KNPR2C9H4G4KQ6MHC4YKM96XRController : Controller
+    {
+        //[Authorize(Roles="Customers")]
+        public ActionResult ValidateCommand(string product, string totalPrice)
+        {
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSandbox"]);
+            var paypal = new PayPalModel(useSandbox);
+
+            paypal.item_name = product;
+            paypal.amount = totalPrice;
+            return View(paypal);
+        }
+
+        public ActionResult RedirectFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult CancelFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult NotifyFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //<add key="business" value="asrce2_1311074442_biz@gmail.com" />
+    }
+}
