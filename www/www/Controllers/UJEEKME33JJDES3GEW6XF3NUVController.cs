53ce41fabfba9088e78336ed50a39b9075b08359
zulldesign <admin@zulldesign.ml>
2014-10-23 14:42:39 +0800

    l

---
+++ www/www/Controllers/UJEEKME33JJDES3GEW6XF3NUVController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace www.Controllers
+{
+    public class UJEEKME33JJDES3GEW6XF3NUVController : Controller
+    {
+        //
+        // GET: /UJEEKME33JJDES3GEW6XF3NUV/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ www/www/Models/UJEEKME33JJDES3GEW6XF3NUVModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace www.Models
+{
+    public class UJEEKME33JJDES3GEW6XF3NUVModel : Controller
+    {
+        //
+        // GET: /UJEEKME33JJDES3GEW6XF3NUVModel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
