f52e8c30b39364c45d812a4a36c1b66535063563
zulldesign <admin@zulldesign.ml>
2014-10-24 08:53:09 +0800

    l

--- www/www/Controllers/UJEEKME33JJDES3GEW6XF3NUVController.cs
+++ www/www/Controllers/UJEEKME33JJDES3GEW6XF3NUVController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using www.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,5 +18,32 @@ namespace www.Controllers
             return View();
         }
 
+        //[Authorize(Roles="Customers")]
+        public ActionResult ValidateCommand(string product, string totalPrice)
+        {
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSandbox"]);
+            var paypal = new UJEEKME33JJDES3GEW6XF3NUVModel(useSandbox);
+
+            paypal.item_name = product;
+            paypal.amount = totalPrice;
+            return View(paypal);
+        }
+
+        public ActionResult RedirectFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult CancelFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult NotifyFromPaypal()
+        {
+            return View();
+        }
+
+        //<add key="business" value="admin@zulldesign.ml" />
     }
 }
--- www/www/Models/UJEEKME33JJDES3GEW6XF3NUVModel.cs
+++ www/www/Models/UJEEKME33JJDES3GEW6XF3NUVModel.cs
@@ -1,20 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Configuration;
 
 namespace www.Models
 {
     public class UJEEKME33JJDES3GEW6XF3NUVModel : Controller
     {
-        //
-        // GET: /UJEEKME33JJDES3GEW6XF3NUVModel/
+        public string cmd { get; set; }
+        public string business { get; set; }
+        public string no_shipping { get; set; }
+        public string @return { get; set; }
+        public string cancel_return { get; set; }
+        public string notify_url { get; set; }
+        public string currency_code { get; set; }
+        public string item_name { get; set; }
+        public string amount { get; set; }
+        public string actionURL { get; set; }
 
-        public ActionResult Index()
+        public UJEEKME33JJDES3GEW6XF3NUVModel(bool useSandbox)
         {
-            return View();
+            this.cmd = "_xclick";
+            this.business = ConfigurationManager.AppSettings["business"];
+            this.cancel_return = ConfigurationManager.AppSettings["cancel_return"];
+            this.@return = ConfigurationManager.AppSettings["return"];
+            if (useSandbox)
+            {
+                this.actionURL = ConfigurationManager.AppSettings["test_url"];
+            }
+            else
+            {
+                this.actionURL = ConfigurationManager.AppSettings["Prod_url"];
+            }
+            // We can add parameters here, for example OrderId, CustomerId, etc....
+            this.notify_url = ConfigurationManager.AppSettings["notify_url"];
+            // We can add parameters here, for example OrderId, CustomerId, etc....
+            this.currency_code = ConfigurationManager.AppSettings["currency_code"];
         }
-
     }
 }
