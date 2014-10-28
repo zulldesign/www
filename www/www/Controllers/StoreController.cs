4d038b05fd12df812dca073c6a1ef657a495c33b
zulldesign <admin@zulldesign.ml>
2014-10-28 18:00:04 +0800

    l

--- www/www/Controllers/StoreController.cs
+++ www/www/Controllers/StoreController.cs
@@ -13,9 +13,16 @@ namespace www.Controllers
         //
         // GET: /Store/
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello from Store.Index()";
+            var genres = new List<Genre> 
+            { 
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+
+            return View(genres);
         }
         //
         // GET: /Store/Browse?genre=Disco
