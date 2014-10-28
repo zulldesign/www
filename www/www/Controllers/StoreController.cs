29991a72c0eb8ab3fcd11963cbdfe51eca049736
zulldesign <admin@zulldesign.ml>
2014-10-28 14:56:48 +0800

    l

---
+++ www/www/Controllers/StoreController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using www.Models;
+
+namespace www.Controllers
+{
+    public class StoreController : Controller
+    {
+        DefaultConnection storeDB = new DefaultConnection();
+
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            var genres = new List<Genre> 
+            { 
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+
+            return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse?genre=Disco
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+
+        //
+        // GET: /Store/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+
+    }
+}
