8b7be7ebf359c0a73aae26088f0a7fd231ae806d
zulldesign <admin@zulldesign.ml>
2014-10-28 18:39:37 +0800

    l

--- www/www/Models/DefaultConnection.cs
+++ www/www/Models/DefaultConnection.cs
@@ -5,6 +5,7 @@ namespace www.Models
     public class DefaultConnection : DbContext
     {
         public DbSet<Album> Albums { get; set; }
-        public DbSet<Genre> Genres { get; set; }        
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }        
     }
 }
