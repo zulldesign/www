731914dea952bcaf8eef064e5b528031a7628044
zulldesign <admin@zulldesign.ml>
2014-10-28 18:15:15 +0800

    l

---
+++ www/www/Models/DefaultConnection.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace www.Models
+{
+    public class DefaultConnection : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }        
+    }
+}
--- www/www/Models/Genre.cs
+++ www/www/Models/Genre.cs
@@ -2,8 +2,11 @@
 
 namespace www.Models
 {
-    public class Genre
+    public partial class Genre
     {
+        public int GenreId { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Album> Albums { get; set; }
     }
 }
