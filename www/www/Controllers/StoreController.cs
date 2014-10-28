99a29dc23d3babcef6e662683e010f61b07be307
zulldesign <admin@zulldesign.ml>
2014-10-28 18:36:44 +0800

    l

--- www/www/Controllers/StoreController.cs
+++ www/www/Controllers/StoreController.cs
@@ -20,19 +20,38 @@ namespace www.Controllers
 
             return View(genres);
         }
+
         //
         // GET: /Store/Browse?genre=Disco
+
         public ActionResult Browse(string genre)
         {
-            var genreModel = new Genre { Name = genre };
+            // Retrieve Genre and its Associated Albums from database
+            var genreModel = storeDB.Genres.Include("Albums")
+                .Single(g => g.Name == genre);
+
             return View(genreModel);
         }
+
         //
         // GET: /Store/Details/5
+
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
+
             return View(album);
+        }
+
+        //
+        // GET: /Store/GenreMenu
+
+        [ChildActionOnly]
+        public ActionResult GenreMenu()
+        {
+            var genres = storeDB.Genres.ToList();
+
+            return PartialView(genres);
         }
 
     }
