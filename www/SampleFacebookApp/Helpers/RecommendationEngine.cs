41f4371bc274c74c7e190daa0e79c99a4b6ab643
zulldesign <admin@zulldesign.ml>
2014-10-29 23:06:49 +0800

    l

---
+++ www/SampleFacebookApp/Helpers/RecommendationEngine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleFacebookApp.Models;
+
+namespace SampleFacebookApp
+{
+    public static class RecommendationEngine
+    {
+        private static string[] categoriesToConsider =
+        {
+            "Entertainment",
+            "Tv show",
+            "Movie",
+            "Book",
+            "Musician/band",
+            "Product/service",
+            "Clothing",
+            "Food/beverages",
+            "Shopping/retail",
+            "Games/toys"
+        };
+
+        public static async Task<List<RecommendedItem>> RecommendProductAsync(MyAppUserFriend friend)
+        {
+            List<RecommendedItem> recommendedItem = new List<RecommendedItem>();
+            if (friend.Likes != null)
+            {
+                var friendLikes = friend.Likes.Data;
+                var friendLikesFilteredByCategory = friendLikes.Where(like => categoriesToConsider.Contains(like.Category));
+
+                foreach (var item in friendLikesFilteredByCategory.Take(10))
+                {
+                    var result = await ShoppingSearchClient.GetProductsAsync(item.Name);
+                    if (result.items == null)
+                    {
+                        // skip empty results
+                        continue;
+                    }
+                    var products = result.items.Select(i => i.product).Take(2).Where(p => p != null);
+                    foreach (var product in products)
+                    {
+                        recommendedItem.Add(new RecommendedItem
+                        {
+                            Product = product,
+                            Reason = "likes " + item.Name
+                        });
+                    }
+                }
+            }
+
+            return recommendedItem;
+        }
+    }
+}
---
+++ www/SampleFacebookApp/Models/SearchResultModel.cs
@@ -0,0 +1,35 @@
+﻿namespace SampleFacebookApp.Models
+{
+    public class SearchResult
+    {
+        public Item[] items { get; set; }
+    }
+
+    public class Item
+    {
+        public Product product { get; set; }
+    }
+
+    public class Product
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Link { get; set; }
+
+        public ProductImage[] Images { get; set; }
+
+        public ProductInventory[] Inventories { get; set; }
+    }
+
+    public class ProductImage
+    {
+        public string link { get; set; }
+    }
+
+    public class ProductInventory
+    {
+        public double Price { get; set; }
+    }
+}
