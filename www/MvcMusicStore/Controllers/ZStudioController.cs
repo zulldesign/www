857ee28c1106672e844518e073dada03c6e3192e
zulldesign <admin@zulldesign.ml>
2014-10-30 11:17:43 +0800

    l

---
+++ www/MvcMusicStore/Controllers/ZStudioController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ZStudioController : Controller
+    {
+        //
+        // GET: /ZStudio/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /ZStudio/Login
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
