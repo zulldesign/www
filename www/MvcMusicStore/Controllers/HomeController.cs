9ee56bf545c3b23003ac3006eb55d042272b8910
zulldesign <admin@zulldesign.ml>
2014-11-01 12:13:58 +0800

    l

--- www/MvcMusicStore/Controllers/HomeController.cs
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -16,5 +16,14 @@ namespace MvcMusicStore.Controllers
             return View();
         }
 
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
