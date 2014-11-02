11302b625a53ee526d46bf04599b32030ce03d4e
zulldesign <admin@zulldesign.ml>
2014-11-02 10:35:02 +0800

    l

--- www/MvcMusicStore/App_Start/BundleConfig.cs
+++ www/MvcMusicStore/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace MvcMusicStore
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquerycarousel").Include(
+                        "~/Scripts/jcarousellite_1.0.1.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
