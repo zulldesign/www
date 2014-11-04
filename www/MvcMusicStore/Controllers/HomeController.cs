90465f0431c955bdd367d1457b3dbbb87575f7a6
zulldesign <admin@zulldesign.ml>
2014-11-04 10:23:20 +0800

    l

---
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var imageList = new
+         System.IO.DirectoryInfo(Server.MapPath("/SlideImages/")).GetFiles();
+            ViewBag.ImageList = imageList;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
