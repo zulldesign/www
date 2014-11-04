77789c599e952bb7a290da5a2098230f2422b576
zulldesign <admin@zulldesign.ml>
2014-11-04 09:53:41 +0800

    l

--- www/SampleFacebookApp/Helpers/ShoppingSearchClient.cs
+++ www/SampleFacebookApp/Helpers/ShoppingSearchClient.cs
@@ -11,7 +11,7 @@ namespace SampleFacebookApp
         private const string SearchApiTemplate = "https://www.googleapis.com/shopping/search/v1/public/products?key={0}&country=US&q={1}&alt=json";
         private static HttpClient client = new HttpClient();
 
-        public static string AppKey = ConfigurationManager.AppSettings["Search:AppKey"];
+        public static string AppKey = ConfigurationManager.AppSettings["uid6336-25643656-80"];
 
         public static Task<SearchResult> GetProductsAsync(string query)
         {
