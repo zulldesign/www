beb97cc966610825baf3b57b4d81493fba032e5b
zulldesign <admin@zulldesign.ml>
2014-11-04 11:42:28 +0800

    l

--- www/MvcMusicStore/Controllers/HomeController.cs
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using Microsoft.AspNet.Mvc.Facebook;
+using Microsoft.AspNet.Mvc.Facebook.Client;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
@@ -24,6 +26,31 @@ namespace MvcMusicStore.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+        [FacebookAuthorize("email", "user_photos")]
+        public async Task<ActionResult> Office(FacebookContext context)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await context.Client.GetCurrentUserAsync<MyAppUser>();
+                return View(user);
+            }
+
+            return View("Error");
+        }
+
+        // This action will handle the redirects from FacebookAuthorizeFilter when 
+        // the app doesn't have all the required permissions specified in the FacebookAuthorizeAttribute.
+        // The path to this action is defined under appSettings (in Web.config) with the key 'Facebook:AuthorizationRedirectPath'.
+        public ActionResult Permissions(FacebookRedirectContext context)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(context);
+            }
+
+            return View("Error");
         }
     }
 }
