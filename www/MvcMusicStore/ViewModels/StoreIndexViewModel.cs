41a7eef5f51e804c1802915cd09d2555ee2124c5
zulldesign <admin@zulldesign.ml>
2014-11-07 17:48:34 +0800

    l

--- www/MvcMusicStore/ViewModels/StoreIndexViewModel.cs
+++ www/MvcMusicStore/ViewModels/StoreIndexViewModel.cs
@@ -18,45 +18,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
-using MvcMusicStore.ViewModels;
 
-namespace MvcMusicStore.Controllers
+namespace MvcMusicStore.ViewModels
 {
-    public class StoreController : Controller
+    public class StoreIndexViewModel
     {
-        //
-        // GET: /Store/
+        public int NumberOfGenres { get; set; }
 
-        public ActionResult Index()
-        {
-            // Create a list of genres
-            var genres = new List<string> { "Rock", "Jazz", "Country", "Pop", "Disco" };
-
-            // Create our view model
-            var viewModel = new StoreIndexViewModel
-            {
-                NumberOfGenres = genres.Count(),
-                Genres = genres
-            };
-
-            return this.View(viewModel);
-        }
-
-        // GET: /Store/Browse?genre=Disco
-        public string Browse(string genre)
-        {
-            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
-
-            return message;
-        }
-
-        // GET: /Store/Details/5
-        public string Details(int id)
-        {
-            string message = "Store.Details, ID = " + id;
-
-            return message;
-        }
+        public List<string> Genres { get; set; }
     }
 }
