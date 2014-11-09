eae737a2e62d3913dd17a55cf8039491c69e2dcc
zulldesign <admin@zulldesign.ml>
2014-11-09 12:18:08 +0800

    l

--- www/MvcMusicStore/Controllers/HomeController.cs
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -19,5 +19,10 @@ namespace MvcMusicStore.Controllers
         {
             return View();
         }
+
+        public ActionResult Store()
+        {
+            return View();
+        }
     }
 }
