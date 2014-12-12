9a6dd24cdb6426cb348f7ecb4270fc1a22a3d5b8
zulldesign <admin@zulldesign.ml>
2014-12-12 10:59:41 +0800

    l

---
+++ www/MvcMusicStore/Controllers/FileUploadController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        //
+        // GET: /FileUpload/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Post method for uploading files
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            try
+            {
+                /*Geting the file name*/
+                string filename = System.IO.Path.GetFileName(file.FileName);
+                /*Saving the file in server folder*/
+                file.SaveAs(Server.MapPath("~/Content/Images/" + filename));
+                string filepathtosave = "Content/Images/" + filename;
+                /*Storing image path to show preview*/
+                ViewBag.ImageURL = filepathtosave;
+                /*
+                 * HERE WILL BE YOUR CODE TO SAVE THE FILE DETAIL IN DATA BASE
+                 *
+                 */
+
+                ViewBag.Message = "File Uploaded successfully.";
+            }
+            catch
+            {
+                ViewBag.Message = "Error while uploading the files.";
+            }
+            return View();
+        }
+
+    }
+}
