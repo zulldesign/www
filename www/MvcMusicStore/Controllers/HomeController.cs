a07238b3093fd53a2a6ad06464a5328315b68bb6
zulldesign <admin@zulldesign.ml>
2014-12-12 11:40:41 +0800

    l

--- www/MvcMusicStore/Controllers/HomeController.cs
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MvcMusicStore.Controllers
         public ActionResult Index()
         {
             // Get most popular albums
-            var albums = GetTopSellingAlbums(5);
+            var albums = GetTopSellingAlbums(10);
 
             return View(albums);
         }        
