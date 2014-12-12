26099a52eb9f43544013b8ef9553903ead4db9fc
zulldesign <admin@zulldesign.ml>
2014-12-12 13:58:54 +0800

    l

---
+++ www/MvcMusicStore/Controllers/AdminController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+   
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        Leedhar_UploadFile _DB = new Leedhar_UploadFile();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadFile()
+        {
+            ViewData["Success"] = "";
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult UploadFile(string Title)
+        {
+            _DB.Upload.Add(new Upload() { Title = Title });
+            _DB.SaveChanges();
+           
+            int Id = (from a in _DB.Upload
+
+                      select a.Upload_id).Max();
+
+            if (Id > 0)
+            {
+                if (Request.Files["file"].ContentLength > 0)
+                {
+                    string extension = System.IO.Path.GetExtension(Request.Files["file"].FileName);
+                    string path1 = string.Format("{0}/{1}{2}", Server.MapPath("~/documents/Files"), Id, extension);
+                    if (System.IO.File.Exists(path1))
+                        System.IO.File.Delete(path1);
+
+                    Request.Files["file"].SaveAs(path1);
+
+
+                }
+                ViewData["Success"] = "Success";
+            }
+            else
+            {
+                ViewData["Success"] = "Upload Failed";
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ www/MvcMusicStore/Models/Leedhar_UploadFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcMusicStore.Models
+{
+    public class Leedhar_UploadFile : DbContext
+    {
+
+        public DbSet<Upload> Upload { get; set; }
+    }
+}
