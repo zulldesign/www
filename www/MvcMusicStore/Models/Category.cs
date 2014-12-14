4ad153cdda87a0d2736e263c81b6a5996d6f0e99
zulldesign <admin@zulldesign.ml>
2014-12-14 11:46:53 +0800

    l

---
+++ www/MvcMusicStore/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> products { get; set; }
+    }
+}
---
+++ www/MvcMusicStore/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Product 
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+
+        public virtual  Category  category { get; set; }
+    }
+}
