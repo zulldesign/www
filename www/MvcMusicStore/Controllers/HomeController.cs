aab7cb912670f07bb6bfe8b1c730e1829da0f300
zulldesign <admin@zulldesign.ml>
2014-12-15 21:30:49 +0800

    l

---
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        public ActionResult Index()
+        {
+            // Get most popular albums
+            var albums = GetTopSellingAlbums(5);
+
+            return View(albums);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult PostToPayPal(string item, string amount)
+        {
+            MvcMusicStore.Models.Paypal paypal = new Models.Paypal();
+            paypal.cmd = "_xclick";
+            paypal.business = ConfigurationManager.AppSettings["BusinessAccountKey"];
+
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["UseSandbox"]);
+            if (useSandbox)
+                ViewBag.actionURl = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+            else
+                ViewBag.actionURl = "https://www.paypal.com/cgi-bin/webscr";
+
+            paypal.cancel_return = System.Configuration.ConfigurationManager.AppSettings["CancelURL"];
+            paypal.@return = ConfigurationManager.AppSettings["ReturnURL"]; //+"&PaymentId=1"; you can append your order Id here
+            paypal.notify_url = ConfigurationManager.AppSettings["NotifyURL"];// +"?PaymentId=1"; to maintain database logic 
+
+            paypal.currency_code = ConfigurationManager.AppSettings["CurrencyCode"];
+
+            paypal.item_name = item;
+            paypal.amount = amount;
+            return View(paypal);
+        }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+    }
+}
