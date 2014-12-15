9185e42816f98948a37c497fe253810f78ab62b4
zulldesign <admin@zulldesign.ml>
2014-12-16 05:49:40 +0800

    l

---
+++ www/MvcMusicStore/Controllers/OrderController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{ 
+    public class OrderController : Controller
+    {
+        private BlogEntities db = new BlogEntities();
+
+        //
+        // GET: /Order/
+
+        public ViewResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+
+        //
+        // GET: /Order/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Order order = db.Orders.Find(id);
+            return View(order);
+        }
+
+        //
+        // GET: /Order/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Order/Create
+
+        [HttpPost]
+        public ActionResult Create(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(order);
+        }
+        
+        //
+        // GET: /Order/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Order order = db.Orders.Find(id);
+            return View(order);
+        }
+
+        //
+        // POST: /Order/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        //
+        // GET: /Order/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Order order = db.Orders.Find(id);
+            return View(order);
+        }
+
+        //
+        // POST: /Order/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
