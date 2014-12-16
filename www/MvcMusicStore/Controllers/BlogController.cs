b71bb11fa29eb974cad201c657513b77f79f1552
zulldesign <admin@zulldesign.ml>
2014-12-16 13:29:57 +0800

    l

--- www/MvcMusicStore/Controllers/BlogController.cs
+++ www/MvcMusicStore/Controllers/BlogController.cs
@@ -1,117 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
-{ 
+{
     public class BlogController : Controller
     {
-        private BlogEntities db = new BlogEntities();
+        BlogEntities blogDB = new BlogEntities();
 
         //
         // GET: /Blog/
 
-        public ViewResult Index()
+        public ActionResult Index()
         {
-            var blogs = db.Blogs.Include(b => b.Product).Include(b => b.Category);
-            return View(blogs.ToList());
+            var categories = blogDB.Categories.ToList();
+
+            return View(categories);
+        }
+
+        //
+        // GET: /Blog/Browse?genre=Disco
+
+        public ActionResult Browse(string category)
+        {
+            // Retrieve Category and its Associated Blogs from database
+            var categoryModel = blogDB.Categories.Include("Blogs")
+                .Single(g => g.Name == category);
+
+            return View(categoryModel);
         }
 
         //
         // GET: /Blog/Details/5
 
-        public ViewResult Details(int id)
+        public ActionResult Details(int id)
         {
-            Blog blog = db.Blogs.Find(id);
+            var blog = blogDB.Blogs.Find(id);
+
             return View(blog);
         }
 
         //
-        // GET: /Blog/Create
+        // GET: /Blog/CategoryMenu
 
-        public ActionResult Create()
+        [ChildActionOnly]
+        public ActionResult CategoryMenu()
         {
-            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name");
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
-            return View();
-        } 
+            var categories = blogDB.Categories.ToList();
 
-        //
-        // POST: /Blog/Create
-
-        [HttpPost]
-        public ActionResult Create(Blog blog)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Blogs.Add(blog);
-                db.SaveChanges();
-                return RedirectToAction("Index");  
-            }
-
-            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name", blog.ProductId);
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", blog.CategoryId);
-            return View(blog);
-        }
-        
-        //
-        // GET: /Blog/Edit/5
- 
-        public ActionResult Edit(int id)
-        {
-            Blog blog = db.Blogs.Find(id);
-            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name", blog.ProductId);
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", blog.CategoryId);
-            return View(blog);
+            return PartialView(categories);
         }
 
-        //
-        // POST: /Blog/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(Blog blog)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(blog).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name", blog.ProductId);
-            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", blog.CategoryId);
-            return View(blog);
-        }
-
-        //
-        // GET: /Blog/Delete/5
- 
-        public ActionResult Delete(int id)
-        {
-            Blog blog = db.Blogs.Find(id);
-            return View(blog);
-        }
-
-        //
-        // POST: /Blog/Delete/5
-
-        [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {            
-            Blog blog = db.Blogs.Find(id);
-            db.Blogs.Remove(blog);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            db.Dispose();
-            base.Dispose(disposing);
-        }
     }
 }
