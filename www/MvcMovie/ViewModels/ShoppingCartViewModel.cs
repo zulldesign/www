467d76b69c8e5bc4c1d338da07ebee5ad5bb4af1
zulldesign <admin@zulldesign.ml>
2014-12-17 19:12:26 +0800

    l

---
+++ www/MvcMovie/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MvcMovie.Models;
+
+namespace MvcMovie.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
