d6a146c5f28a3c4fdbc5704ebde9d78648344d1f
zulldesign <admin@zulldesign.ml>
2014-12-18 02:21:42 +0800

    l

---
+++ www/MvcMovie/Models/Leedhar_MvcMovie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Leedhar_MvcMovie : DbContext
+    {
+
+        public DbSet<Upload> Upload { get; set; }
+    }
+}
---
+++ www/MvcMovie/Models/Upload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Upload
+    {
+        [Key]
+        public virtual int Upload_id { get; set; }
+        [Required]
+        public virtual string Title { get; set; }
+
+    }
+}
