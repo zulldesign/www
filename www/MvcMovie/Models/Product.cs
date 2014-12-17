629d0eb13c57bbc7d88835b07868b3cb114398d2
zulldesign <admin@zulldesign.ml>
2014-12-17 22:33:19 +0800

    l

---
+++ www/MvcMovie/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcMusicStore.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
