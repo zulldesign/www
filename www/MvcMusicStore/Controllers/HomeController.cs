eea54c77bb6802fb9b1219b0ea83749c016eb512
zulldesign <admin@zulldesign.ml>
2014-12-17 19:48:10 +0800

    l

---
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
