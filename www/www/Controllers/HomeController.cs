0ca39fabe9cd9201c9af04665ab62c959552fa96
zulldesign <admin@zulldesign.ml>
2014-12-22 02:31:17 +0800

    l

--- www/www/Controllers/HomeController.cs
+++ www/www/Controllers/HomeController.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using www.Models;
 
 namespace www.Controllers
 {
     public class HomeController : Controller
     {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        public ActionResult Index()
+        {
+            // Get most popular albums
+            var albums = GetTopSellingAlbums(5);
+
+            return View(albums);
+        }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
         /// <summary>
         /// Multiple file upload with asp.net mvc and HTML5
         /// </summary>
         /// <returns></returns>
-        public ActionResult Index()
+        public ActionResult About()
         {
             return View();
         }
@@ -22,7 +43,7 @@ namespace www.Controllers
         /// <param name="files"></param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase[] files)
+        public ActionResult About(HttpPostedFileBase[] files)
         {
             try
             {
