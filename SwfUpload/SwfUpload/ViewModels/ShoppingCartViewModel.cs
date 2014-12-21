490ecac172f0f2b6e633019832e541af56a335df
zulldesign <admin@zulldesign.ml>
2014-12-21 23:41:29 +0800

    l

---
+++ SwfUpload/SwfUpload/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SwfUpload.Models;
+
+namespace SwfUpload.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
