cc41bebc2d855094f28f74f6fde80b75aff570e2
zulldesign <admin@zulldesign.ml>
2014-12-22 00:44:03 +0800

    l

--- www/www/Controllers/HomeController.cs
+++ www/www/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using www.Models;
 
 namespace www.Controllers
 {
     public class HomeController : Controller
     {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
         /// <summary>
         /// Multiple file upload with asp.net mvc and HTML5
         /// </summary>
