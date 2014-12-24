c880ee1a8bb987695c1b15874b40261e65a8cddc
zulldesign <admin@zulldesign.ml>
2014-12-24 11:40:53 +0800

    l

--- www/MvcMusicStore/Controllers/HomeController.cs
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using MvcMusicStore.Models;
 
@@ -7,9 +9,6 @@ namespace MvcMusicStore.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         MusicStoreEntities storeDB = new MusicStoreEntities();
 
         public ActionResult Index()
@@ -30,5 +29,43 @@ namespace MvcMusicStore.Controllers
                 .Take(count)
                 .ToList();
         }
+        /// <summary>
+        /// Multiple file upload with asp.net mvc and HTML5
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult About()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Post method for uploading files
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult About(HttpPostedFileBase[] files)
+        {
+            try
+            {
+                /*Lopp for multiple files*/
+                foreach (HttpPostedFileBase file in files)
+                {
+                    /*Geting the file name*/
+                    string filename = System.IO.Path.GetFileName(file.FileName);
+                    /*Saving the file in server folder*/
+                    file.SaveAs(Server.MapPath("~/Content/Images/" + filename));
+                    string filepathtosave = "Content/Images/" + filename;
+                    /*HERE WILL BE YOUR CODE TO SAVE THE FILE DETAIL IN DATA BASE*/
+                }
+
+                ViewBag.Message = "File Uploaded successfully.";
+            }
+            catch
+            {
+                ViewBag.Message = "Error while uploading the files.";
+            }
+            return View();
+        }
+
     }
 }
