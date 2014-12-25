37ea5d21a1d911e0793d15ec1cd1dfd998c2f9b1
zulldesign <admin@zulldesign.ml>
2014-12-25 13:43:13 +0800

    l

--- www/MvcMusicStore/Controllers/HomeController.cs
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
@@ -65,6 +66,29 @@ namespace MvcMusicStore.Controllers
                 ViewBag.Message = "Error while uploading the files.";
             }
             return View();
+        }
+
+        public ActionResult PostToPayPal(string item, string amount)
+        {
+            MvcMusicStore.Models.Paypal paypal = new Models.Paypal();
+            paypal.cmd = "_xclick";
+            paypal.business = ConfigurationManager.AppSettings["BusinessAccountKey"];
+
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["UseSandbox"]);
+            if (useSandbox)
+                ViewBag.actionURl = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+            else
+                ViewBag.actionURl = "https://www.paypal.com/cgi-bin/webscr";
+
+            paypal.cancel_return = System.Configuration.ConfigurationManager.AppSettings["CancelURL"];
+            paypal.@return = ConfigurationManager.AppSettings["ReturnURL"]; //+"&PaymentId=1"; you can append your order Id here
+            paypal.notify_url = ConfigurationManager.AppSettings["NotifyURL"];// +"?PaymentId=1"; to maintain database logic 
+
+            paypal.currency_code = ConfigurationManager.AppSettings["CurrencyCode"];
+
+            paypal.item_name = item;
+            paypal.amount = amount;
+            return View(paypal);
         }
 
     }
