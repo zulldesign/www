b93deb9386124685a4d2e8db12d0c1f6e4d761ed
zulldesign <admin@zulldesign.ml>
2014-12-25 13:17:18 +0800

    l

---
+++ www/MvcMusicStore/Controllers/PayPalController.cs
@@ -0,0 +1,46 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class PayPalController : Controller
+    {
+        //[Authorize(Roles="Customers")]
+        public ActionResult ValidateCommand(string product, string totalPrice)
+        {
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSandbox"]);
+            var paypal = new PayPalModel(useSandbox);
+
+            paypal.item_name = product;
+            paypal.amount = totalPrice;
+            return View(paypal);
+        }
+
+        public ActionResult RedirectFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult CancelFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult NotifyFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+         //<add key="business" value="asrce2_1311074442_biz@gmail.com" />
+    }
+}
