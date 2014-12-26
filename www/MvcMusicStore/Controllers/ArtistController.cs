5e2e498dbabc595f95eff96fa5a4db2bc2e3ba5e
zulldesign <admin@zulldesign.ml>
2014-12-26 21:05:23 +0800

    l

---
+++ www/MvcMusicStore/Controllers/ArtistController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    [Authorize]
+    public class ArtistController : Controller
+    {
+        private MusicStoreEntities db = new MusicStoreEntities();
+
+        //
+        // GET: /Artist/
+
+        public ViewResult Index()
+        {
+            return View(db.Artists.ToList());
+        }
+
+        //
+        // GET: /Artist/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            return View(artist);
+        }
+
+        //
+        // GET: /Artist/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Artist/Create
+
+        [HttpPost]
+        public ActionResult Create(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Artists.Add(artist);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(artist);
+        }
+        
+        //
+        // GET: /Artist/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            return View(artist);
+        }
+
+        //
+        // POST: /Artist/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artist);
+        }
+
+        //
+        // GET: /Artist/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Artist artist = db.Artists.Find(id);
+            return View(artist);
+        }
+
+        //
+        // POST: /Artist/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Artist artist = db.Artists.Find(id);
+            db.Artists.Remove(artist);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
