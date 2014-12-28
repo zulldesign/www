265a045f777cd35ca3185a8dc340c4cb910952a9
zulldesign <admin@zulldesign.ml>
2014-12-28 23:41:58 +0800

    l

--- www/MvcMusicStore/Controllers/HomeController.cs
+++ www/MvcMusicStore/Controllers/HomeController.cs
@@ -19,5 +19,34 @@ namespace MvcMusicStore.Controllers
         {
             return View();
         }
+        /// <summary>
+        /// Post method for uploading files
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult About(HttpPostedFileBase[] files)
+        {
+            try
+            {
+                /*Lopp for multiple files*/
+                foreach (HttpPostedFileBase file in files)
+                {
+                    /*Geting the file name*/
+                    string filename = System.IO.Path.GetFileName(file.FileName);
+                    /*Saving the file in server folder*/
+                    file.SaveAs(Server.MapPath("~/Content/Images/" + filename));
+                    string filepathtosave = "Content/Images/" + filename;
+                    /*HERE WILL BE YOUR CODE TO SAVE THE FILE DETAIL IN DATA BASE*/
+                }
+
+                ViewBag.Message = "File Uploaded successfully.";
+            }
+            catch
+            {
+                ViewBag.Message = "Error while uploading the files.";
+            }
+            return View();
+        }
     }
 }
