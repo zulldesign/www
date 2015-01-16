e76e012dc81bf9cc3049caee88420cac66e346dd
zulldesign <admin@zulldesign.ml>
2015-01-16 22:45:22 +0800

    l

---
+++ wp-content/themes/comicpress/content-aside.php
@@ -0,0 +1,10 @@
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="post-content">
+		<div class="entry">
+			<?php comicpress_display_the_content(); ?>
+			<div class="clear"></div>
+			<?php edit_post_link(__('Edit this post.','comicpress'), '', ''); ?>
+		</div>
+	</div>
+	<div class="clear"></div>
+</div>
---
+++ wp-content/themes/comicpress/sidebar-left.php
@@ -0,0 +1,16 @@
+<div id="sidebar-left">
+	<div class="sidebar">
+	<?php 
+		do_action('comicpress-sidebar-left');
+		if ( !dynamic_sidebar('sidebar-left-sidebar') ) { ?>
+		<div class="sidebar-no-widgets">
+			<?php _e('There are currently no widgets assigned to the left-sidebar, place some!','comicpress'); ?><br />
+			<br />
+			<?php _e('Once you add widgets to this sidebar, this default information will go away.','comicpress'); ?><br />
+			<br />
+			<?php _e('Widgets can be added by going to your dashboard (wp-admin) -> Appearance -> Widgets, drag a widget you want to see into one of the appropriate sidebars.','comicpress'); ?><br />
+		</div>
+		<?php }
+	?>
+	</div>
+</div>
