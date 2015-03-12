0b4e402eb0ed52d9de63bc58e41aabd4cd41cb34
zulldesign <admin@zulldesign.ml>
2015-03-12 11:39:36 +0800

    l

---
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        BlogEntities blogDB = new BlogEntities();
+
+        public ActionResult Index()
+        {
+            // Get most popular blogs
+            var blogs = GetTopSellingBlogs(5);
+
+            return View(blogs);
+        }
+
+        private List<Blog> GetTopSellingBlogs(int count)
+        {
+            // Group the order details by blog and return
+            // the blogs with the highest count
+
+            return blogDB.Blogs
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Post method for uploading files
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult About(HttpPostedFileBase[] files)
+        {
+            try
+            {
+                /*Lopp for multiple files*/
+                foreach (HttpPostedFileBase file in files)
+                {
+                    /*Geting the file name*/
+                    string filename = System.IO.Path.GetFileName(file.FileName);
+                    /*Saving the file in server folder*/
+                    file.SaveAs(Server.MapPath("~/Content/Images/" + filename));
+                    string filepathtosave = "Content/Images/" + filename;
+                    /*HERE WILL BE YOUR CODE TO SAVE THE FILE DETAIL IN DATA BASE*/
+                }
+
+                ViewBag.Message = "File Uploaded successfully.";
+            }
+            catch
+            {
+                ViewBag.Message = "Error while uploading the files.";
+            }
+            return View();
+        }
+    }
+}
---
+++ MvcMusicStore/Controllers/OrderDetailController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{ 
+    public class OrderDetailController : Controller
+    {
+        private BlogEntities db = new BlogEntities();
+
+        //
+        // GET: /OrderDetail/
+
+        public ViewResult Index()
+        {
+            var orderdetails = db.OrderDetails.Include(o => o.Blog).Include(o => o.Order);
+            return View(orderdetails.ToList());
+        }
+
+        //
+        // GET: /OrderDetail/Details/5
+
+        public ViewResult Details(int id)
+        {
+            OrderDetail orderdetail = db.OrderDetails.Find(id);
+            return View(orderdetail);
+        }
+
+        //
+        // GET: /OrderDetail/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.BlogId = new SelectList(db.Blogs, "BlogId", "Title");
+            ViewBag.OrderId = new SelectList(db.Orders, "OrderId", "Username");
+            return View();
+        } 
+
+        //
+        // POST: /OrderDetail/Create
+
+        [HttpPost]
+        public ActionResult Create(OrderDetail orderdetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OrderDetails.Add(orderdetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.BlogId = new SelectList(db.Blogs, "BlogId", "Title", orderdetail.BlogId);
+            ViewBag.OrderId = new SelectList(db.Orders, "OrderId", "Username", orderdetail.OrderId);
+            return View(orderdetail);
+        }
+        
+        //
+        // GET: /OrderDetail/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            OrderDetail orderdetail = db.OrderDetails.Find(id);
+            ViewBag.BlogId = new SelectList(db.Blogs, "BlogId", "Title", orderdetail.BlogId);
+            ViewBag.OrderId = new SelectList(db.Orders, "OrderId", "Username", orderdetail.OrderId);
+            return View(orderdetail);
+        }
+
+        //
+        // POST: /OrderDetail/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(OrderDetail orderdetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderdetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BlogId = new SelectList(db.Blogs, "BlogId", "Title", orderdetail.BlogId);
+            ViewBag.OrderId = new SelectList(db.Orders, "OrderId", "Username", orderdetail.OrderId);
+            return View(orderdetail);
+        }
+
+        //
+        // GET: /OrderDetail/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            OrderDetail orderdetail = db.OrderDetails.Find(id);
+            return View(orderdetail);
+        }
+
+        //
+        // POST: /OrderDetail/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            OrderDetail orderdetail = db.OrderDetails.Find(id);
+            db.OrderDetails.Remove(orderdetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
