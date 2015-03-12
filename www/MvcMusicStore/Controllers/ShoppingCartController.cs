38efe4a340d4abdcbbf451771c491b1999f578bb
zulldesign <admin@zulldesign.ml>
2015-03-12 11:32:41 +0800

    l

---
+++ www/MvcMusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using MvcMusicStore.ViewModels;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        BlogEntities blogDB = new BlogEntities();
+
+        //
+        // GET: /ShoppingCart/
+
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Set up our ViewModel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+
+            // Return the view
+            return View(viewModel);
+        }
+
+        //
+        // GET: /Blog/AddToCart/5
+
+        public ActionResult AddToCart(int id)
+        {
+
+            // Retrieve the blog from the database
+            var addedBlog = blogDB.Blogs
+                .Single(blog => blog.BlogId == id);
+
+            // Add it to the shopping cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(addedBlog);
+
+            // Go back to the main blog page for more shopping
+            return RedirectToAction("Index");
+        }
+
+        //
+        // AJAX: /ShoppingCart/RemoveFromCart/5
+
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            // Remove the item from the cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Get the name of the blog to display confirmation
+            string blogName = blogDB.Carts
+                .Single(item => item.RecordId == id).Blog.Title;
+
+            // Remove from cart
+            int itemCount = cart.RemoveFromCart(id);
+
+            // Display the confirmation message
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(blogName) +
+                    " has been removed from your shopping cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+
+            return Json(results);
+        }
+
+        //
+        // GET: /ShoppingCart/CartSummary
+
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+
+            return PartialView("CartSummary");
+        }
+    }
+}
