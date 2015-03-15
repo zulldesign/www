979ed3b40074f77830e94faadfff5399cacb091e
zulldesign <admin@zulldesign.ml>
2015-03-16 01:24:14 +0800

    l

---
+++ www/MvcMusicStore/Controllers/TekunController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace MvcMusicStore.Controllers
+{
+    public class TekunController : Controller
+    {
+        //
+        // GET: /Tekun/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PostToPayPal(string item, string amount)
+        {
+            MvcMusicStore.Models.Tekun paypal = new Models.Tekun();
+            paypal.cmd = "_xclick";
+            paypal.business = ConfigurationManager.AppSettings["BusinessAccountKey"];
+
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["UseSandbox"]);
+            if (useSandbox)
+                ViewBag.actionURl = "https://MvcMusicStore.sandbox.paypal.com/cgi-bin/webscr";
+            else
+                ViewBag.actionURl = "https://MvcMusicStore.paypal.com/cgi-bin/webscr";
+
+            paypal.cancel_return = System.Configuration.ConfigurationManager.AppSettings["CancelURL"];
+            paypal.@return = ConfigurationManager.AppSettings["ReturnURL"]; //+"&PaymentId=1"; you can append your order Id here
+            paypal.notify_url = ConfigurationManager.AppSettings["NotifyURL"];// +"?PaymentId=1"; to maintain database logic 
+
+            paypal.currency_code = ConfigurationManager.AppSettings["CurrencyCode"];
+
+            paypal.item_name = item;
+            paypal.amount = amount;
+            return View(paypal);
+        }
+    }
+}
